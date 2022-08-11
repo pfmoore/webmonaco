69a0304907c963210fde9ed55017bdae807f0fe6
Paul Moore <p.f.moore@gmail.com>
2022-08-11 17:09:50 +0100

    Switch to static page for UI and web service backend

--- webmonaco/__init__.py
+++ webmonaco/__init__.py
@@ -19,10 +19,8 @@ if sys.platform == "linux":
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-    if request.method == "POST":
-        return show_results(request)
     return render_template("index.html")
 
 @app.route("/run", methods=['GET', 'POST'])
@@ -60,75 +58,6 @@ def test():
         proc = subprocess.run(command, capture_output=True, input="", encoding="utf-8", cwd=d)
 
         return {"returncode": proc.returncode, "stdout": proc.stdout, "command": cmdfile.read_text()}
-
-@app.route("/monaco")
-def monaco():
-    command = [str(monaco_bin)] + parse_options(request.args)
-    proc = subprocess.run(command, stdout=subprocess.PIPE, encoding="utf-8")
-    return jsonify(dict(stdout=proc.stdout))
-
-
-CHECKBOXES = [
-    "statistics",
-    "histogram",
-    "cumulative",
-    "rcumulative",
-    "percent",
-    "probability",
-    "exact",
-    "new",
-]
-def parse_options(args):
-    options = []
-    from pprint import pprint
-    pprint(args)
-    for checkbox in CHECKBOXES:
-        if args.get(checkbox):
-            options.append("-" + checkbox)
-
-    other_options = args.get("other_options")
-    if other_options:
-        # Note: shlex.split is awful, it reads stdin if
-        # you pass None as input. Avoid that or the webapp
-        # will hang!
-        other_options = shlex.split(other_options)
-        options.extend(other_options)
-
-    expr = args.get('expr')
-    # Monaco doesn't like newlines in expressions
-    expr = expr.replace("\n","")
-    options.append(expr)
-
-    iterations = args.get("iterations")
-    other_iterations = args.get("other_iterations")
-    if iterations == "other":
-        iterations = other_iterations
-
-    iterations = int(iterations)
-    if not args.get("exact"):
-        options.append(str(iterations))
-
-    return options
-
-def show_results(request):
-    if request.method == 'POST':  #this block is only entered when the form is submitted
-        try:
-            options = parse_options(request.form)
-        except Exception as e:
-            print("OOPS", e)
-            return "Got an error: " + traceback.format_exc()
-
-        p = subprocess.run(
-            [str(monaco_bin)] + options,
-            capture_output=True,
-            text=True
-        )
-        return f'''<h1>Command: {options}</h1>
-        Return code: {p.returncode}<br>
-        <pre>
-        {p.stdout}
-        </pre>
-        '''
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", "8080"))
