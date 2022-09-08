7c1048a5292b00b46ba47e369d58646fd94917f4
Paul Moore <p.f.moore@gmail.com>
2022-09-08 10:56:53 +0100

    Add a simple script to get the latest executables

---
+++ get_assets.py
@@ -0,0 +1,30 @@
+import io
+import json
+import sys
+from pathlib import Path
+from urllib.request import Request, urlopen
+from zipfile import ZipFile
+
+API_URL = "https://api.github.com/repos/pfmoore/builder-monaco/releases"
+
+if len(sys.argv) < 2:
+    raise SystemExit("Must provide a tag name")
+tag = sys.argv[1]
+
+with urlopen(Request(API_URL, headers={"Accept": "application/vnd.github+json"})) as f:
+    data = json.load(f)
+
+for r in data:
+    if r["tag_name"] != tag:
+        continue
+    for asset in r["assets"]:
+        name = asset["name"]
+        print(name)
+        with urlopen(asset["browser_download_url"]) as a:
+            content = io.BytesIO(a.read())
+        z = ZipFile(content)
+        print(z.namelist())
+        if name.startswith("monaco-windows"):
+            Path("bin/win32/monaco.exe").write_bytes(z.read("monaco.exe"))
+        if name.startswith("monaco-linux"):
+            Path("bin/linux/monaco").write_bytes(z.read("monaco"))
