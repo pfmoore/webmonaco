ab31838b222170e056140810e1a3ece2131bfdc4
Paul Moore <p.f.moore@gmail.com>
2025-02-04 12:37:10 +0000

    Add timeout and output limiting for Monaco subprocess

--- app/webmonaco.py
+++ app/webmonaco.py
@@ -1,6 +1,8 @@
 import subprocess
 import sys
 import tempfile
+from concurrent.futures import ThreadPoolExecutor
+from dataclasses import dataclass, asdict
 from pathlib import Path
 
 from flask import Flask, request
@@ -11,6 +13,17 @@ monaco_bin = (Path(__file__).parent / f"bin/{sys.platform}/monaco").absolute()
 app = Flask(__name__)
 CORS(app)
 
+@dataclass
+class StreamResult:
+    truncated: bool
+    content: str
+
+@dataclass
+class ProcessResult:
+    status: str
+    rc: int
+    out: StreamResult
+    err: StreamResult
 
 @app.route("/")
 def index():
@@ -28,6 +41,54 @@ def favicon():
 def help():
     with app.open_resource("help.html") as f:
         return f.read()
+
+def read_stream(fd, limit) -> StreamResult:
+    # Try to read an extra byte to detect truncation If the process outputs
+    # *exactly* `limit` characters, and then waits indefinitely before
+    # outputting more, the timeout of the process will make this look like we
+    # didn't truncate. This isn't expected to be a problem in practice.
+    content = fd.read(limit+1)
+    truncated = len(content) > limit
+    return StreamResult(truncated, content)
+
+def terminate_after(proc, seconds):
+    try:
+        proc.wait(seconds)
+        return "Completed", proc.returncode
+    except subprocess.TimeoutExpired:
+        proc.kill()
+        return "Timeout", -1
+
+# Limits
+TIMEOUT = 60     # Maximum runtime (60 seconds)
+LIMIT = 100_000  # Maximum output size (100K characters)
+
+def run_and_capture_output(cmd, cwd):
+    proc = subprocess.Popen(
+        cmd,
+        stdin=subprocess.DEVNULL,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        encoding="utf-8",
+        text=True,
+        cwd=cwd
+    )
+
+    with ThreadPoolExecutor() as exc:
+        proc_result = exc.submit(terminate_after, proc, TIMEOUT)
+        out, err = exc.map(read_stream, (proc.stdout, proc.stderr), (LIMIT, LIMIT))
+        status, rc = proc_result.result()
+
+    return ProcessResult(status, rc, out, err)
+
+
+# Result format example:
+# {
+#     'status': 'Completed',
+#     'rc': 0,
+#     'out': {'truncated': False, 'content': 'abc'},
+#     'err': {'truncated': False, 'content': 'def'}
+# }
 
 @app.route("/run", methods=["GET", "POST"])
 def run():
@@ -62,17 +123,9 @@ def run():
             print("-nofiles -noread -noprompt +stderr", file=f)
             print(file_content, file=f)
         command = [str(monaco_bin), cmdfile.name]
-        proc = subprocess.run(
-            command, capture_output=True, input="", encoding="utf-8", cwd=d
-        )
+        result = run_and_capture_output(command, d)
 
-        return {
-            "returncode": proc.returncode,
-            "stdout": proc.stdout,
-            "stderr": proc.stderr,
-            "command": cmdfile.read_text(),
-        }
-
+        return asdict(result)
 
 # if __name__ == "__main__":
 #     port = int(os.getenv("PORT", "8080"))
